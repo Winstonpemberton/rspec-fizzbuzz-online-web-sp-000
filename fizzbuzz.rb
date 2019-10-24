3c352c0bca2d798ca9cd5381c6ec11cbe151cf19
Winston Pemberton <winstonpemberton@gmail.com>
2019-10-24 21:43:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(input)
-  if input % 3 == 0
+  if ((input % 3 == 0) && (input % 5 == 0))
     "Fizz"
   elsif input % 5 == 0
     "Buzz"
-  elsif ((input % 3 == 0) && (input % 5 == 0))
+  elsif input % 3 == 0
      "FizzBuzz"
   else
     nil
